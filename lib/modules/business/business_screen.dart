42091f6cea1d20ced4da151364f65be14976a814
makdad-raee <makdadraeeeeee@gmail.com>
2023-10-29 15:36:43 +0300

    create sport busniness and science  screens

---
+++ lib/modules/business/business_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'BusinessScreen',
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
